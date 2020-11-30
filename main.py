f1208f19eb0037fc8db8ba64a2e844fa7d2a93fd
DEN <dvgv35@gmail.com>
2020-11-30 19:24:40 +0300

    login dialog

--- main.py
+++ main.py
@@ -1,16 +1,37 @@
 import sqlite3
-import sy
+import sys
 import datetime
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QLCDNumber, QLabel, QMainWindow, QDialog
 from PyQt5 import uic
-con = sqlite3.connect('14. Защита проекта QT/db.sqlite')
-cur = con.cursor()
 
 
 class MainProgram(QMainWindow):
-    def __init__(self):
+    def __init__(self, login, password):
         super().__init__()
         uic.loadUi('ui.ui', self)
+
+    def checkPassword(self, login, password):
+        pass
+
+
+class LoginDialog(QDialog):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('dui.ui', self)
+        self.buttonBox.accepted.connect(self.runMainProgram)
+
+    def runMainProgram(self):
+        login = self.login.text()
+        password = self.password.text()
+        main_prog = MainProgram(login, password)
+        main_prog.show()
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    login_dial = LoginDialog()
+    login_dial.show()
+    sys.exit(app.exec_())
 
     # cur.execute(
     #     """insert into teacher(name, login, password) values('ddfjdfgj', 'fjfgjfg', 'fjfjfjf')""")
